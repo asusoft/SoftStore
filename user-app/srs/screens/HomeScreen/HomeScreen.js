9b49cee31c5fae25eb8fe817efb1add50229a3b9
Asusoft <asusoft19a@gmail.com>
2023-09-19 22:10:27 +0300

    greetings added to home screen

--- user-app/srs/screens/HomeScreen/HomeScreen.js
+++ user-app/srs/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,15 @@
 //import liraries
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView, Pressable, Image } from 'react-native';
-import { COLORS } from '../../../assets/constants/theme';
+import { COLORS, SIZES } from '../../../assets/constants/theme';
 import Header from '../../components/Header';
 import { SimpleLineIcons } from "@expo/vector-icons";
 import images from '../../../assets/constants/images';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 // create a component
 const HomeScreen = () => {
+    const { dbUser } = useAuthContext();
     return (
         <SafeAreaView style={styles.container}>
             <Header
@@ -41,6 +43,9 @@ const HomeScreen = () => {
                     </Pressable>
                 }
             />
+            <View style={styles.greetings}>
+                <Text style={styles.greetingsText}>{dbUser?.name.split(' ')[0]}, nice to see you again!</Text>
+            </View>
         </SafeAreaView>
     );
 };
@@ -50,6 +55,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: COLORS.background,
+    },
+    greetings: {
+        marginHorizontal: SIZES.padding + 5,
+        marginTop: 10
+    },
+    greetingsText: {
+        fontSize: 22,
+        fontWeight: '600'
     },
     location: {
         flexDirection: "row",
