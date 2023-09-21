deb8eeb349ff9fcd0ff498d5f29d8df9e1868793
Asusoft <asusoft19a@gmail.com>
2023-09-21 17:37:36 +0300

    set active opacity to item

--- user-app/srs/components/Item.js
+++ user-app/srs/components/Item.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import dummyData from '../../assets/constants/dummyData';
 import { COLORS } from '../../assets/constants/theme';
 
@@ -10,11 +10,11 @@ const Item = ({ item }) => {
     const filteredImage = itemImages.filter(image => image.itemId == item.id);
     const itemImage = filteredImage[0].uri[0];
     return (
-        <View style={{ alignItems: 'center', }}>
+        <TouchableOpacity activeOpacity={1} onPress={() => console.log(item.name)} style={{ alignItems: 'center', }}>
             <Image source={{ uri: itemImage }} style={{ height: 200, width: "100%", marginTop: 10 }} resizeMode='contain' />
             <Text style={{ marginTop: 30, fontSize: 24, fontWeight: '700', marginHorizontal: 20, textAlign: 'center' }}>{item.name}</Text>
             <Text style={{ marginTop: 10, fontSize: 16, color: COLORS.primary }}>From ₦ 1,200,000</Text>
-        </View>
+        </TouchableOpacity>
     );
 };
 
