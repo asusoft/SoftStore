1efd81a4565256e7f2c03f1a000b8a53b7a7465a
Asusoft <asusoft19a@gmail.com>
2023-09-25 16:55:23 +0300

    Brand details screen header

--- AdminPanel/srs/Screens/pages/BrandDetails.js
+++ AdminPanel/srs/Screens/pages/BrandDetails.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Image, Text, Pressable, StyleSheet, Button } from 'react-native';
 import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 import images from '../../../assets/constants/images';
+import icons from '../../../assets/constants/icons';
+import { NavLink, Outlet } from 'react-router-dom';
 import { COLORS } from '../../../assets/constants/theme';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -9,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 const BrandDetails = () => {
     const db = getFirestore();
     const { brandName } = useParams();
-    const [brandDetails, setBrandDetails] = React.useState(null);
+    const [brand, setBrandDetails] = React.useState(null);
 
     const navigate = useNavigate();
 
@@ -40,10 +42,25 @@ const BrandDetails = () => {
 
     return (
         <View style={styles.container}>
-            <Text>{brandDetails?.name}</Text>
-            <Image source={{ uri: brandDetails?.icon }} resizeMode='contain' style={{ height: 100, width: 300 }} />
+            <View style={{ width: '100%', height: 220, backgroundColor: COLORS.white, borderRadius: 8 }}>
+                <View style={{ height: '50%', width: "100", backgroundColor: COLORS.lightGray, borderTopLeftRadius: 8, borderTopRightRadius: 8 }} />
+                <View style={{ padding: 10, position: 'absolute', top: 25, left: 25, height: 170, borderWidth: 1, borderColor: COLORS.black, backgroundColor: COLORS.white, width: 170, borderRadius: 8 }}>
+                    <Image source={{ uri: brand?.icon }} style={{ height: "100%", width: "100%" }} />
+                </View>
 
-            <Button onPress={() => goBack()} style={{ height: 30, width: 30, }} />
+                <View style={{ marginLeft: 220, marginTop: 10, flexDirection: 'row', justifyContent: 'space-between', marginEnd: 20 }}>
+                    <View>
+                        <Text style={{ fontSize: 24, fontWeight: '600' }}>{brand?.name}</Text>
+                        <Text style={{ fontSize: 18, marginTop: 8 }}>6 Products</Text>
+                    </View>
+                    <NavLink to='createbrand'>
+                        <View style={{ flexDirection: 'row', padding: 8, backgroundColor: COLORS.darkPrimary, borderRadius: 6 }}>
+                            <Image source={icons.plus} style={{ height: 20, width: 20, tintColor: COLORS.white }} />
+                            <Text style={{ fontSize: 16, color: COLORS.white, marginLeft: 4 }}>Add Product</Text>
+                        </View>
+                    </NavLink>
+                </View>
+            </View>
         </View>
     );
 };
@@ -52,7 +69,7 @@ const BrandDetails = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 25
+        padding: 35
     },
 });
 
